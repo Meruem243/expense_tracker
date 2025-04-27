bcf183f7ed49a50fe1013cf6e0360fca259764af
Mughees <mughees.ahmed243@gmail.com>
2025-04-27 23:12:30 +0500

    Add formatted date to Expense model and update ExpenseItem widget to display it

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -20,9 +20,9 @@ class ExpenseItem extends StatelessWidget {
                 const Spacer(),
                 Row(
                   children: [
-                    Icon(Icons.access_time),
+                    Icon(categoryIcons[expense.category]),
                     const SizedBox(width: 8),
-                    Text(expense.date.toString()),
+                    Text(expense.formattedDate),
                   ],
                 ),
               ],
