4bf3b0d7d8c7a828b270a000be17aa6e0b9f0177
Mughees <mughees.ahmed243@gmail.com>
2025-04-29 23:01:57 +0500

    Add amount input field to NewExpense widget and update save functionality

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,10 +14,12 @@ class _NewExpenseState extends State<NewExpense> {
   // }
 
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -33,14 +35,34 @@ class _NewExpenseState extends State<NewExpense> {
             maxLength: 50,
             decoration: const InputDecoration(label: Text('Title')),
           ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: _amountController,
+            // maxLength: 50,
+            decoration: const InputDecoration(
+              prefix: Text('\$ '),
+              label: Text('Amount'),
+            ),
+            // keyboardType: const TextInputType.numberWithOptions(decimal: true),
+            keyboardType: TextInputType.number,
+          ),
+          const SizedBox(height: 16),
           Row(
             children: [
               ElevatedButton(
                 onPressed: () {
                   // print(_enteredTitle);
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
                 child: Text('Save Expense'),
+              ),
+              const SizedBox(width: 16),
+              OutlinedButton(
+                onPressed: () {
+                  //Navigator.pop(context);
+                },
+                child: Text('Cancel'),
               ),
             ],
           ),
